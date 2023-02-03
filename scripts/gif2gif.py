51efdf12e13d4195194a662e31c5528e33516e8d
LonicaMewinsky <timmahw@gmail.com>
2023-02-03 13:19:54 -0600

    Fixed random seed handling

--- scripts/gif2gif.py
+++ scripts/gif2gif.py
@@ -5,6 +5,7 @@ import modules.images
 import gradio as gr
 import numpy as np
 import tempfile
+import random
 from PIL import Image, ImageSequence
 from modules.processing import Processed, process_images
 from modules.shared import state
@@ -39,12 +40,14 @@ class Script(scripts.Script):
         self.gif_name = str()
         self.orig_fps = 0
         self.orig_duration = 0
+        self.orig_total_seconds = 0
         self.orig_n_frames = 0
         self.orig_dimensions = (0,0)
         self.ready = False
         self.desired_fps = 0
         self.desired_interp = 0
         self.desired_duration = 0
+        self.desired_total_seconds = 0
         self.slowmo = False
         self.gif2gifdir = tempfile.TemporaryDirectory()
         self.img2img_component = gr.Image()
@@ -70,10 +73,20 @@ class Script(scripts.Script):
                             interp_slider = gr.Slider(label = "Interpolation frames", value = 0)
                             gif_resize = gr.Checkbox(value = True, label="Resize result back to original dimensions")
                             gif_clear_frames = gr.Checkbox(value = True, label="Delete intermediate frames after GIF generation")
-                    with gr.Column():
-                        with gr.Box():
-                            fps_actual = gr.Slider(1, 50, step = 1, interactive = False, label = "Actual FPS")
-       
+                            gif_common_seed = gr.Checkbox(value = True, label="For -1 seed, all frames in a GIF have common seed")
+                    with gr.Column():   
+                        with gr.Row():
+                            with gr.Box():
+                                with gr.Column():
+                                    fps_actual = gr.Textbox(value="", interactive = False, label = "Actual FPS")
+                                    seconds_actual = gr.Textbox(value="", interactive = False, label = "Actual total duration")
+                                    frames_actual = gr.Textbox(value="", interactive = False, label = "Actual total frames")
+                            with gr.Box():
+                                with gr.Column():
+                                    fps_original = gr.Textbox(value="", interactive = False, label = "Original FPS")
+                                    seconds_original = gr.Textbox(value="", interactive = False, label = "Original total duration")
+                                    frames_original = gr.Textbox(value="", interactive = False, label = "Original total frames")
+                                    
         #Control functions
         def processgif(gif):
             try:
@@ -82,9 +95,10 @@ class Script(scripts.Script):
                 self.orig_dimensions = init_gif.size
                 self.orig_duration = init_gif.info["duration"]
                 self.orig_n_frames = init_gif.n_frames
-                self.orig_fps = round(1000 / int(init_gif.info["duration"]))
+                self.orig_total_seconds = round((self.orig_duration * self.orig_n_frames)/1000, 2)
+                self.orig_fps = round(1000 / int(init_gif.info["duration"]), 2)
                 self.ready = True
-                return init_gif, gif.name, gr.Image.update(visible = True), self.orig_fps
+                return init_gif, gif.name, gr.Image.update(visible = True), self.orig_fps, self.orig_fps, (f"{self.orig_total_seconds} seconds"), self.orig_n_frames
             except:
                 print(f"Failed to load {gif.name}. Not a valid animated GIF?")
                 return None
@@ -99,25 +113,27 @@ class Script(scripts.Script):
             if (self.ready and fps and (interp_frames != None)):
                 self.desired_fps = fps
                 self.desired_interp = interp_frames
-                calcdur = (1000 / fps) / (interp_frames+1)
+                total_n_frames = self.orig_n_frames + ((self.orig_n_frames -1) * self.desired_interp)
+                calcdur = (1000 / fps) / (total_n_frames/self.orig_n_frames)
                 if calcdur < 20:
                     calcdur = 20
                     self.slowmo = True
                 self.desired_duration = calcdur
+                self.desired_total_seconds = round((self.desired_duration * total_n_frames)/1000, 2)
                 gifbuffer = (f"{self.gif2gifdir.name}/previewgif.gif")
                 previewgif = Image.open(self.gif_name)
                 previewgif.save(gifbuffer, format="GIF", save_all=True, duration=self.desired_duration, loop=0)
                 if interp:
                     interp(gifbuffer, self.desired_interp, self.desired_duration)
-                return gifbuffer, 1000/self.desired_duration
+                return gifbuffer, round(1000/self.desired_duration, 2), f"{self.desired_total_seconds} seconds", total_n_frames
         
         #Control change events
-        fps_slider.change(fn=fpsupdate, inputs = [fps_slider, interp_slider], outputs = [display_gif, fps_actual])
-        interp_slider.change(fn=fpsupdate, inputs = [fps_slider, interp_slider], outputs = [display_gif, fps_actual])
-        upload_gif.upload(fn=processgif, inputs = upload_gif, outputs = [self.img2img_component, display_gif, display_gif, fps_slider])
+        fps_slider.change(fn=fpsupdate, inputs = [fps_slider, interp_slider], outputs = [display_gif, fps_actual, seconds_actual, frames_actual])
+        interp_slider.change(fn=fpsupdate, inputs = [fps_slider, interp_slider], outputs = [display_gif, fps_actual, seconds_actual, frames_actual])
+        upload_gif.upload(fn=processgif, inputs = upload_gif, outputs = [self.img2img_component, display_gif, display_gif, fps_slider, fps_original, seconds_original, frames_original])
         upload_gif.change(fn=cleargif, inputs = upload_gif, outputs = display_gif)
 
-        return [gif_resize, gif_clear_frames]
+        return [gif_resize, gif_clear_frames, gif_common_seed]
 
     #Grab the img2img image component for change later
     #Maybe there's a better way to do this?
@@ -127,15 +143,13 @@ class Script(scripts.Script):
             return self.img2img_component
     
     #Main run
-    def run(self, p, gif_resize, gif_clear_frames, *args):
+    def run(self, p, gif_resize, gif_clear_frames, gif_common_seed, *args):
         try:
             inp_gif = Image.open(self.gif_name)
         except:
             print("Something went wrong with GIF. Processing still from img2img.")
             proc = process_images(p)
             return proc
-        #TODO: Add logic for seeds. Same seed every set? Iterate?
-        print(p.batch_size)
         return_images = []
         all_prompts = []
         infotexts = []
@@ -153,6 +167,9 @@ class Script(scripts.Script):
                 state.skipped = False
             if state.interrupted:
                 break
+            #If the seed is -1, make sure all frames have same random
+            if(gif_common_seed and (p.seed == -1)):
+                p.seed = random.randrange(100000000, 999999999)
             for frame in ImageSequence.Iterator(inp_gif):
                 if state.skipped:
                     state.skipped = False
@@ -165,7 +182,6 @@ class Script(scripts.Script):
                 inter_images += proc.images
                 all_prompts += proc.all_prompts
                 infotexts += proc.infotexts
-            print(len(inter_images))
             if(gif_resize):
                 for i in range(len(inter_images)):
                     inter_images[i] = inter_images[i].resize(self.orig_dimensions)
